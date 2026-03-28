9c5ece1890143ac046f0c3b6a643f8bcc3667539
Rishil Ramesh <rishil.ramesh@outlook.com>
2026-03-28 19:30:00 +0530

    Change : Update Logs schema to use default values for log creation date and time values

--- src/db/models.py
+++ src/db/models.py
@@ -1,6 +1,11 @@
 from datetime import datetime, date, time
+from zoneinfo import ZoneInfo
 from sqlalchemy import String, Date, Time, DateTime
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+# IST Timezone
+IST = ZoneInfo("Asia/Kolkata")
 
 
 class Base(DeclarativeBase):
@@ -11,7 +16,7 @@ class Logs(Base):
     __tablename__ = "logs"
     id: Mapped[int] = mapped_column(primary_key=True)
     entry: Mapped[str] = mapped_column(String(1000))
-    date_of_creation: Mapped[date] = mapped_column(Date)
-    time_of_creation: Mapped[time] = mapped_column(Time)
-    created_at: Mapped[datetime] = mapped_column(DateTime)
-    updated_at: Mapped[datetime | None] = mapped_column(DateTime, nullable=True)
+    date_of_creation: Mapped[date] = mapped_column(Date, default=lambda: datetime.now(tz=IST).date())
+    time_of_creation: Mapped[time] = mapped_column(Time, default=lambda: datetime.now(tz=IST).time())
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(tz=IST))
+    updated_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), nullable=True)
