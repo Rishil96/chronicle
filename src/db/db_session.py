9b8227d1eeafe8e7c10440b86e057459f28fae7a
Rishil Ramesh <rishil.ramesh@outlook.com>
2026-03-28 18:26:32 +0530

    Add : Database Session creation class

---
+++ src/db/db_session.py
@@ -0,0 +1,36 @@
+import os
+from contextlib import contextmanager
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from src.db.models import Base
+
+
+class DatabaseSession:
+
+    SQLITE_PREFIX = "sqlite:///"
+    DATABASE_URI = "chronicle.db"
+
+    def __init__(self):
+        load_dotenv()
+        directory_path = os.getenv("SQLITE_DATABASE_URL")
+        database_url = f"{self.SQLITE_PREFIX}{directory_path}{self.DATABASE_URI}"
+        self.engine = create_engine(database_url, connect_args={"check_same_thread": False})
+        self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+        # Create table on app startup if it doesn't exist
+        Base.metadata.create_all(bind=self.engine)
+
+    @contextmanager
+    def get_session(self):
+        """
+        Creates a database session that is used as a context manager
+        """
+        session = self.SessionLocal()
+        try:
+            yield session
+            session.commit()
+        except:
+            session.rollback()
+            raise
+        finally:
+            session.close()
