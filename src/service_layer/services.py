632fe031390de6b190304c7a1e1b93a486dccebe
Rishil Ramesh <rishil.ramesh@outlook.com>
2026-03-29 10:57:47 +0530

    Add : Function to delete log and improvements in existing service functions for easier debugging

--- src/service_layer/services.py
+++ src/service_layer/services.py
@@ -1,14 +1,16 @@
 """
 This module contains all operations to be performed on the Chronicle logging app
 """
-from datetime import date, timedelta
+from datetime import datetime, date, timedelta
 from sqlalchemy import select
 from sqlalchemy.orm import Session
+from zoneinfo import ZoneInfo
 from src.db.models import Logs
 from src.db.db_session import DatabaseSession
 
 # Database session
 db = DatabaseSession()
+IST = ZoneInfo("Asia/Kolkata")
 
 
 def add_log(session: Session, log_entry: str, use_llm: bool = True) -> None:
@@ -20,7 +22,7 @@ def add_log(session: Session, log_entry: str, use_llm: bool = True) -> None:
     session.commit()
 
 
-def get_logs(session: Session, single_date: date | None = None, from_date: date | None = None, to_date: date | None = None):
+def get_logs(session: Session, single_date: date | None = None, from_date: date | None = None, to_date: date | None = None) -> list[Logs]:
     """
     Function to read logs using 2 modes of filtering
         1. Single date
@@ -43,14 +45,26 @@ def get_logs(session: Session, single_date: date | None = None, from_date: date 
     return list(result)
 
 
-def update_log(session: Session, log_id: int, updated_log_entry: str, use_llm: bool = True):
+def update_log(session: Session, log_id: int, updated_log_entry: str, use_llm: bool = True) -> bool:
     """
     Function to update a log entry using ID
     """
-    log_to_update = session.execute(select(Logs).where(Logs.id == log_id)).scalars().one()
-    log_to_update.entry = updated_log_entry
-    session.commit()
+    log_to_update = session.execute(select(Logs).where(Logs.id == log_id)).scalars().one_or_none()
+    if log_to_update:
+        log_to_update.entry = updated_log_entry
+        log_to_update.updated_at = datetime.now(tz=IST)
+        session.commit()
+        return True
+    return False
 
 
 def delete_log(session: Session, log_id: int):
-    raise NotImplementedError
+    """
+    Function to delete a log entry using ID
+    """
+    log_to_delete = session.execute(select(Logs).where(Logs.id == log_id)).scalars().one_or_none()
+    if log_to_delete:
+        session.delete(log_to_delete)
+        session.commit()
+        return True
+    return False
