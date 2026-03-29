5ea4721a0a4c343a002c8684d4abd3d664f070bc
Rishil Ramesh <rishil.ramesh@outlook.com>
2026-03-29 08:49:14 +0530

    Add : Function logic to add log to database

--- src/service_layer/services.py
+++ src/service_layer/services.py
@@ -1,13 +1,23 @@
 """
 This module contains all operations to be performed on the Chronicle logging app
 """
-from datetime import date
+from datetime import datetime, date
+from sqlalchemy import select
 from sqlalchemy.orm import Session
+from src.db.models import Logs
+from src.db.db_session import DatabaseSession
+
+# Database session
+db = DatabaseSession()
 
 
 def add_log(session: Session, log_entry: str, use_llm: bool = True):
-    raise NotImplementedError
-
+    """
+    Function to add a log entry to the Chronicle app database
+    """
+    new_log = Logs(entry=log_entry)
+    session.add(new_log)
+    session.commit()
 
 def get_logs(session: Session, single_date: date | None = None, from_date: date | None = None, to_date: date | None = None):
     raise NotImplementedError
