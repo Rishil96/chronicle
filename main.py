7bd8cfbb4fbbd0d52a9c56e23dc334bcd37df808
Rishil Ramesh <rishil.ramesh@outlook.com>
2026-03-30 19:52:52 +0530

    Add : Routes for each HTML page and empty HTML files for the Chronicle app

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import logging
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from src.logger import setup_logger
 
 
+# Logger Setup
 setup_logger()
 logger = logging.getLogger("chronicle")
 
 
 app = FastAPI()
+# Setup templating and static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
 @app.get("/")
-def home():
-    return {
-        "message": "Chronicle App!"
-    }
+def home(request: Request):
+    return templates.TemplateResponse(request,"home.html", {"request": request})
+
+
+@app.get("/history")
+def history(request: Request):
+    return templates.TemplateResponse(request,"history.html", {"request": request})
+
+
+@app.get("/filter")
+def filter_logs(request: Request):
+    return templates.TemplateResponse(request,"filter.html", {"request": request})
