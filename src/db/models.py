eca4a9355edd53b7e1c114452f5650f19517be51
Rishil Ramesh <rishil.ramesh@outlook.com>
2026-03-31 22:42:13 +0530

    Change : Modify time formatting to show HH:MM with AM/PM

--- src/db/models.py
+++ src/db/models.py
@@ -26,7 +26,7 @@ class Logs(Base):
             "id": self.id,
             "entry": self.entry,
             "date_of_creation": self.date_of_creation,
-            "time_of_creation": self.time_of_creation.strftime("%H:%M:%S"),
+            "time_of_creation": self.time_of_creation.strftime("%I:%M %p"),
             "created_at": self.created_at,
             "updated_at": self.updated_at
         }
