d9a9fa2087993eea5f780e5b95473090877a0b4b
Rishil Ramesh <rishil.ramesh@outlook.com>
2026-03-31 20:36:40 +0530

    Add : Route to handle log insertion via home page

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import logging
 from datetime import date
 from dotenv import load_dotenv
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from src.db import DatabaseSession
 from src.logger import setup_logger
+from src.service_layer import add_log
 from src.utils import greet_user
 
 # Load environment variables
@@ -14,6 +16,8 @@ load_dotenv()
 setup_logger()
 logger = logging.getLogger("chronicle")
 
+# Database session setup
+db = DatabaseSession()
 
 app = FastAPI()
 # Setup templating and static files
@@ -40,3 +44,12 @@ def history(request: Request):
 @app.get("/filter")
 def filter_logs(request: Request):
     return templates.TemplateResponse(request,"filter.html", {"request": request})
+
+
+@app.post("/logs")
+def add_logs(log_entry: str = Form(...)):
+    """
+    Route to add a log entry via HTMX from Home route
+    """
+    with db.get_session() as session:
+        add_log(session=session, log_entry=log_entry)
